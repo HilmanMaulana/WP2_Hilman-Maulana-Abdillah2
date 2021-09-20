c6c76b763edee5b31ecd1c1528396aa9c5785323
HilmanMaulana <hilmanmaulana0502@gmail.com>
2021-09-20 07:23:43 +0700

    Upload file dalam controllers Contoh1.php,index.html,Welcome.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Hilman Maulana Abdillah <br />
+        Saya tinggal di daerah Bogor Selatan <br />
+        Olah raga yang saya sukai adalah Bulutangkis.";
+    }
+}
